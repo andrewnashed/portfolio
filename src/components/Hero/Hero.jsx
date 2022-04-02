1029091351617efc0641fe5fbaaaee81147cfeb1
Andrew <cyberandrew30@yahoo.com>
2022-04-02 14:59:44 -0400

    fixed typos in the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,10 +26,10 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, iam'}{' '}
-            <span className="text-color-main">{name || ''}</span>
+            {title || 'Hi, my name is'}{' '}
+            <span className="text-color-main">{`${name},` || ''}</span>
             <br />
-            {subtitle || "I'm a Full Stack Developer."}
+            {subtitle || "and I am a Full-Stack developer based in NYC."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
